17606794b7ec2bed61be6a809da8f104bf209d6e
Chance Smith <chancesmith97@hotmail.com>
2019-02-22 22:47:30 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
 def my_collect(array)
   new_arr = []
   i = 0 
-  while array.length > i 
-  yield(array[i])
+  while array.length > i
+    new_arr << yield(array[i])
     i = i + 1 
   end
+  new_arr
 end
 
